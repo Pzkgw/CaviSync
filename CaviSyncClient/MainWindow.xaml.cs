f1ee99ee871ea2a5fe4f9ee86ce7fbc89e888a01
Visoiu Bogdan <panzerfaust2006@yahoo.com>
2017-03-02 16:47:42 +0200

    T1

--- CaviSyncClient/MainWindow.xaml.cs
+++ CaviSyncClient/MainWindow.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.NetworkInformation;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using MainLib;
 
 namespace CaviSyncClient
@@ -32,6 +22,8 @@ namespace CaviSyncClient
 
             btnDirRefresh_Click(null, null);
 
+            textBoxIPServer.Text = "10.10.10.15";
+
             //        Utils.ExecuteCommand(
             //System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory() +
             //"\\InstallUtil.exe /i /unattended " +
@@ -42,7 +34,7 @@ namespace CaviSyncClient
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             // curatenie de primavara
-            textBox1.TextChanged -= TextBox_TextChanged;
+           
         }
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -64,6 +56,9 @@ namespace CaviSyncClient
 
         private void btnService_Click(object sender, RoutedEventArgs e)
         {
+            if (!ServerIpAddressDoesAnswer()) return;          
+
+            return;
 
             if (!Exec.SerIsOn(serviceName))
             {
@@ -110,12 +105,48 @@ namespace CaviSyncClient
 
         }
 
+        private bool ServerIpAddressDoesAnswer()
+        {
+            //IPAddress ad=null;            IPAddress.TryParse(textBoxIPServer.Text, out ad);
+            //if(textBoxIPServer.Text.Length<7 || textBoxIPServer.Text.Length > 15 ||
+            //    textBoxIPServer.Text.Contains("000") || textBoxIPServer.Text.Count(c => c == '.') != 3)
+            //{
+            //    MessageBox.Show("Ip address textbox string not valid");
+            //        return;
+            //}
+
+            IPAddress ipServer = null;
+            PingReply reply = null;
+
+            try
+            {
+                IPAddress.TryParse(textBoxIPServer.Text, out ipServer);//textBoxIPServer.Text "125.100.0.15"
+                reply = (new Ping()).Send(ipServer);
+            }
+            catch (Exception) { }
+
+            if (reply != null && reply.Status == IPStatus.Success)
+            {
+                //("Address: {0}", reply.Address.ToString());
+                //("RoundTrip time: {0}", reply.RoundtripTime);
+                //("Time to live: {0}", reply.Options.Ttl);
+                //("Don't fragment: {0}", reply.Options.DontFragment);
+                //("Buffer size: {0}", reply.Buffer.Length);
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(string.Format(" Server IP does not answer.{0}{0} Reply status: {1}",
+                    Environment.NewLine, (reply == null) ? " no reply" : reply.Status.ToString()));
+                return false;
+            }
+        }
+
         /// <summary>
         /// Refresh all GUI data
         /// </summary>
         private void btnDirRefresh_Click(object sender, RoutedEventArgs e)
         {
-            textBox1.TextChanged -= TextBox_TextChanged;
 
             {
                 IPAddress ip = Utils.GetLocalIpAddress();
@@ -124,22 +155,19 @@ namespace CaviSyncClient
 
             SetServiceGui(Exec.SerIsOn(serviceName));
 
-            textBox1.Text = RegEdit.ServerGetPath();
+            textBox1.Content = RegEdit.ServerGetPath();
 
-            btnDirUpdate.Visibility = Visibility.Collapsed;
-
-            textBox1.TextChanged += TextBox_TextChanged;
 
         }
 
         private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
-            btnDirUpdate.Visibility = Visibility.Visible;
+
         }
 
         private void btnDirUpdate_Click(object sender, RoutedEventArgs e)
         {
-            RegEdit.ServerUpdate(textBox1.Text);
+            //RegEdit.ServerUpdate(textBox1.Content.ToString());
         }
 
         private void btnSerDel_Click(object sender, RoutedEventArgs e)
--- MainLib/Optiuni.cs
+++ MainLib/Optiuni.cs
@@ -7,7 +7,8 @@ namespace MainLib
     {
         public const int EndpointPort = 5000;
         public const string EndpointType = "net.tcp";
-        public static IPAddress EndpointIP = IPAddress.Parse("10.10.10.15");
+        public static string EndpointIP = "10.10.10.15";
+        //public static IPAddress EndpointIP = IPAddress.Parse("10.10.10.15");
 
         public const string
             regPath = "SOFTWARE\\Wow6432Node\\GTS Global Intelligence",
